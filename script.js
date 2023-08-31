53aa31d1300a26d47c84d362efb4e08505b64d30
dinukah <dinukahamarasinghe@gmail.com>
2023-08-31 14:54:21 +0530

    changes

--- script.js
+++ script.js
@@ -79,3 +79,61 @@ btnNext2.addEventListener('click', e => {
 btnPrev2.addEventListener('click', e => {
     slider2.go('-1');
 });
+
+
+// Disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false);
+        });
+})();
+
+
+// Get all the phone input field elements
+const phoneInputFields = document.querySelectorAll("input[type='tel']");
+
+// Loop through all the phone input fields
+phoneInputFields.forEach(phoneInputField => {
+    // Initialize the phone input with preferred countries
+    const phoneInput = window.intlTelInput(phoneInputField, {
+        preferredCountries: ["gb", "us"],
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+
+    //deny enter letters and spcial characters
+    phoneInputField.addEventListener("beforeinput", event => {
+        const input = event.data;
+        if (event.inputType === "insertText" && !/^[0-9]+$/.test(input)) {
+            event.preventDefault();
+        }
+    });
+
+    // Add a submit event listener to the parent form element
+    phoneInputField.form.addEventListener("submit", () => {
+        // Get the selected country data
+        const selectedCountryData = phoneInput.getSelectedCountryData();
+
+        // Get the input value in the E.164 format for the selected country
+        const inputNumber = phoneInput.getNumber(intlTelInputUtils.numberFormat.E164);
+
+        // Get the country code from the input number
+        const countryCode = inputNumber.substring(1, selectedCountryData.dialCode.length + 1);
+
+        // Set the input field value to the formatted phone number and the country code
+        phoneInputField.value = `${phoneInput.getNumber()}`;
+    });
+});
